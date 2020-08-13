afe3f717ac70810bcae19494a20ff04d10b83f6f
Coalery <doralife12@naver.com>
2020-08-14 08:43:03 +0900

    Add: 004

---
+++ _004/_004.js
@@ -0,0 +1,11 @@
+var a = 1;
+console.log(a);
+var a = 2;
+console.log(a);
+
+var name = '안녕!';
+
+var letter = `Dear ${name} \
+powagjpowaegjopawegjopawegjopawjgeopacoawmeksmdlkvmzkldf. ${name} fjewaoopokzdmvzkzdm. ${1+1} zlszlsskjfowepaoowaeapo. ${name}`;
+
+console.log(letter);
