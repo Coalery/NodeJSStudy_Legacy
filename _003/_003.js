18534b4798229d81f54a1e928629d26c3c34f15d
Coalery <doralife12@naver.com>
2020-08-13 17:13:23 +0900

    Add: _003

---
+++ _003/_003.js
@@ -0,0 +1,10 @@
+// Numbers
+console.log(1 + 1);
+console.log(4 - 1);
+console.log(3 * 7);
+console.log(10 / 2);
+
+// Strings
+console.log('1' + '1');
+console.log('100' + '1');
+console.log('apwoegjopawgejpoawejgopawgejopaweg'.length);
